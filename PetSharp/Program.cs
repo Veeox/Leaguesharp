7fa9d8644feac1ffbf1a5f0264df580354749bca
Veeox <rorivo@gmail.com>
2015-05-17 13:08:31 +0800

    Random Name Gen Added, also drawing menu fixed Kappa

--- PetSharp/Program.cs
+++ PetSharp/Program.cs
@@ -39,6 +39,13 @@ namespace PetSharp
         public static int Lvl;
         public static string PetName;
 
+        //Random Name Gen
+        static string[] NameDatabase1 = { "Ba", "Bax", "Dan", "Fi", "Fix", "Fiz", "Gi", "Gix", "Giz", "Gri", "Gree", "Greex", "Grex", "Ja", "Jax", "Jaz", "Jex", "Ji", "Jix", "Ka", "Kax", "Kay", "Kaz", "Ki", "Kix", "Kiz", "Klee", "Kleex", "Kwee", "Kweex", "Kwi", "Kwix", "Kwy", "Ma", "Max", "Ni", "Nix", "No", "Nox", "Qi", "Rez", "Ri", "Ril", "Rix", "Riz", "Ro", "Rox", "So", "Sox", "Vish", "Wi", "Wix", "Wiz", "Za", "Zax", "Ze", "Zee", "Zeex", "Zex", "Zi", "Zix", "Zot" };
+
+        static string[] NameDatabase2 = { "b", "ba", "be", "bi", "d", "da", "de", "di", "e", "eb", "ed", "eg", "ek", "em", "en", "eq", "ev", "ez", "g", "ga", "ge", "gi", "ib", "id", "ig", "ik", "im", "in", "iq", "iv", "iz", "k", "ka", "ke", "ki", "m", "ma", "me", "mi", "n", "na", "ni", "q", "qa", "qe", "qi", "v", "va", "ve", "vi", "z", "za", "ze", "zi", "", "", "", "", "", "", "", "", "", "", "", "", "" };
+
+        static string[] NameDatabase3 = { "ald", "ard", "art", "az", "azy", "bit", "bles", "eek", "eka", "et", "ex", "ez", "gaz", "geez", "get", "giez", "iek", "igle", "ik", "il", "in", "ink", "inkle", "it", "ix", "ixle", "lax", "le", "lee", "les", "lex", "lyx", "max", "maz", "mex", "mez", "mix", "miz", "mo", "old", "rax", "raz", "reez", "rex", "riez", "tee", "teex", "teez", "to", "uek", "x", "xaz", "xeez", "xik", "xink", "xiz", "xonk", "yx", "zeel", "zil" };
+
         //Drag and Baroon Stuff
         private static string DragonBuff = "s5test_dragonslayerbuff";
         private static string BaroonBuff = "exaltedwithbaronnashor";
@@ -92,8 +99,9 @@ namespace PetSharp
             Menu.SubMenu("shop").AddItem(new MenuItem("food3", "Buy Food 3").SetValue(false));
 
             //Draw menu
-            Menu.AddItem(new MenuItem("draw", "Drawings"));
-            Menu.SubMenu("draw").AddItem(new MenuItem("Drawstats", "Draw Stats").SetValue(true));
+            Menu.AddSubMenu(new Menu("Drawings", "draw"));
+            Menu.SubMenu("draw").AddItem(new MenuItem("drawstats", "Draw Stats").SetValue(true));
+            Menu.SubMenu("draw").AddItem(new MenuItem("drawsprites", "Draw Sprites").SetValue(true));
 
             //Track menu
             Menu.AddItem(new MenuItem("track", "Track Game").SetValue(true));
@@ -108,15 +116,17 @@ namespace PetSharp
 
         static void Drawing_OnDraw(EventArgs args)
         {
-            if (Menu.Item("drawstats").GetValue<bool>())
+            if (!Menu.Item("drawstats").GetValue<bool>() || !Menu.Item("track").GetValue<bool>())
             {
-                var xpos = 1660;
-                var ypos = 680;
-                Drawing.DrawText(xpos, ypos, System.Drawing.Color.LightSkyBlue, "PetSharp v" + Ver + " by Veeox");
-                Drawing.DrawText(xpos, ypos + 20, System.Drawing.Color.LightSkyBlue, "Pet Name: " + PetName);
-                Drawing.DrawText(xpos, ypos + 40, System.Drawing.Color.LightSkyBlue, "Level: " + (int)Lvl);
-                Drawing.DrawText(xpos, ypos + 60, System.Drawing.Color.LightSkyBlue, "XP: " + (int)CurXP + "/" + (int)MaxXP);
+                return;
             }
+            var xpos = 1660;
+            var ypos = 680;
+
+            Drawing.DrawText(xpos, ypos, System.Drawing.Color.LightSkyBlue, "PetSharp v" + Ver + " by Veeox");
+            Drawing.DrawText(xpos, ypos + 20, System.Drawing.Color.LightSkyBlue, "Pet Name: " + PetName);
+            Drawing.DrawText(xpos, ypos + 40, System.Drawing.Color.LightSkyBlue, "Level: " + (int)Lvl);
+            Drawing.DrawText(xpos, ypos + 60, System.Drawing.Color.LightSkyBlue, "XP: " + (int)CurXP + "/" + (int)MaxXP);
         }
 
         private static void OnUpdate(EventArgs args)
@@ -138,6 +148,7 @@ namespace PetSharp
 
         }
 
+        //Initialize all the things!
         private static void InitEvents()
         {
             Game.OnUpdate += OnUpdate;
@@ -146,7 +157,6 @@ namespace PetSharp
             Drawing.OnDraw += Drawing_OnDraw;
         }
 
-        //Need to fix RAGEEEEE
         private static void OnGameNotify(GameNotifyEventArgs args)
         {
             var killer = args.NetworkId;
@@ -262,10 +272,10 @@ namespace PetSharp
 
         private static void FirstRun()
         {
+            RandomName();
             Lvl = 1;
             CurXP = 0;
             MaxXP = 100;
-            PetName = "TestName";
             ConvertInt(Lvl, CurXP, MaxXP);
         }
 
@@ -429,6 +439,14 @@ namespace PetSharp
         private static void EndScore()
         {
             return;
+        }
+
+        //Name Gen Stuff
+        private static void RandomName()
+        {
+            Random RandName = new Random();
+            string Temp = NameDatabase1[RandName.Next(0, NameDatabase1.Length)] + NameDatabase2[RandName.Next(0, NameDatabase2.Length)];
+            PetName = Temp;
         }
 
     }
